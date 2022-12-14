a04d4f30bd1514d5f50f025804bc897db379eee3
DandaraF <dandara-chr@hotmail.com>
2022-12-13 21:59:08 -0300

    create banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="main-container">
+  <div>
+    <Banner/>
+    <div class="main-container">
       <h1>Monte o seu burger:</h1>
-    
+    </div>
   </div>
 </template>
 
 <script>
 
+import Banner from '../components/Banner'
+
 export default {
   name: 'Home',
+  components:{
+    Banner
+  }
 
 }
 </script>
